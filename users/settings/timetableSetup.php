0b173d11fa538789d0a9a82b51559e9e6810271f
Rindula <rindola@t-online.de>
2017-09-13 15:40:41 +0200

    ---

--- users/settings/timetableSetup.php
+++ users/settings/timetableSetup.php
@@ -1,7 +1,7 @@
 <?php
     $zeiten = array("07:30 - 08:15", "08:15 - 09:00", "09:15 - 10:00", "10:00 - 10:45", "11:00 - 11:45", "11:45 - 12:30", "12:30 - 13:15", "13:15 - 14:00", "14:00 - 14:45", "15:00 - 15:45", "15:45 - 16:30");
     $userConn->query("CREATE TABLE IF NOT EXISTS `timetable_".$_SESSION["userid"]."` ( `id` INT NOT NULL AUTO_INCREMENT , `montag` INT , `dienstag` INT , `mittwoch` INT , `donnerstag` INT , `freitag` INT , PRIMARY KEY (`id`)) ENGINE = InnoDB;");
-    if ($userConn->query("SELECT * FROM timetable_" . $_SESSION["userid"] . " WHERE id=$n") === false) {
+    if ($userConn->query("SELECT * FROM timetable_" . $_SESSION["userid"]) === false) {
         $userConn->query("ALTER TABLE timetable_" . $_SESSION["userid"] . " AUTO_INCREMENT = 1");
         foreach ($zeiten as $key) {
             $userConn->query("INSERT INTO timetable_" . $_SESSION["userid"] . " DEFAULT VALUES");
