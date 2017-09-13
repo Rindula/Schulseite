b2a14e5d64bad679e4903be8a4798b9de6402ef5
Rindula <rindola@t-online.de>
2017-09-13 15:34:06 +0200

    ---

--- users/settings/timetableSetup.php
+++ users/settings/timetableSetup.php
@@ -1,6 +1,12 @@
 <?php
-    $userConn->query("CREATE TABLE IF NOT EXISTS `timetable_".$_SESSION["userid"]."` ( `id` INT NOT NULL AUTO_INCREMENT , `montag` INT NOT NULL , `dienstag` INT NOT NULL , `mittwoch` INT NOT NULL , `donnerstag` INT NOT NULL , `freitag` INT NOT NULL , PRIMARY KEY (`id`)) ENGINE = InnoDB;");
     $zeiten = array("07:30 - 08:15", "08:15 - 09:00", "09:15 - 10:00", "10:00 - 10:45", "11:00 - 11:45", "11:45 - 12:30", "12:30 - 13:15", "13:15 - 14:00", "14:00 - 14:45", "15:00 - 15:45", "15:45 - 16:30");
+    $userConn->query("CREATE TABLE IF NOT EXISTS `timetable_".$_SESSION["userid"]."` ( `id` INT NOT NULL AUTO_INCREMENT , `montag` INT , `dienstag` INT , `mittwoch` INT , `donnerstag` INT , `freitag` INT , PRIMARY KEY (`id`)) ENGINE = InnoDB;");
+    if ($userConn->query("SELECT * FROM timetable_" . $_SESSION["userid"] . " WHERE id=$n") === false) {
+        $userConn->query("ALTER TABLE timetable_" . $_SESSION["userid"] . " AUTO_INCREMENT = 1");
+        foreach ($zeiten as $key) {
+            $userConn->query("INSERT INTO timetable_" . $_SESSION["userid"] . " DEFAULT VALUES");
+        }
+    }
 ?>
 
 <div class="content">
@@ -20,7 +26,7 @@
             $n = 0;
             foreach ($zeiten as $z) {
                 $n++;
-                $afafaf = $userConn->query("SELECT * FROM timetable_" . $_SESSION["userid"] . "WHERE id=$n");
+                $afafaf = $userConn->query("SELECT * FROM timetable_" . $_SESSION["userid"] . " WHERE id=$n");
                 $we = $afafaf->fetch_array();
                 $i = 0;
                 foreach ($woche as $w) {
