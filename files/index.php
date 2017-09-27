2a9fff223326e950b8a9eba7c2c48e490eff482c
Rindula <rindola@t-online.de>
2017-09-27 21:46:52 +0200

    Debug

--- files/index.php
+++ files/index.php
@@ -10,12 +10,13 @@ $files = scandir("content");
 foreach ($files as $key => $value) {
     if($value != ".." || $value != ".") {
         $s = explode("_", explode(".", $value)[0]);
-        $i = $s[0];
-        $n = $s[1];
-        $sql = "SELECT name FROM flist WHERE id = '$i'";
-        $ret = $conn->query($sql);
-        $r = $ret->fetch_assoc();
-        $i = $r["name"];
-        echo "$i - $n";
+        print_r($s);
+        // $i = $s[0];
+        // $n = $s[1];
+        // $sql = "SELECT name FROM flist WHERE id = '$i'";
+        // $ret = $mysqli->query($sql);
+        // $r = $ret->fetch_assoc();
+        // $i = $r["name"];
+        // echo "$i - $n";
     }
 }
