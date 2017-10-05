0f8088008ece90ac55e343fd66ce1cc05e0e1d61
Unknown <contact@rindula.de>
2017-10-06 00:08:35 +0200

    Kontakt improvements

--- kontakt/index.php
+++ kontakt/index.php
@@ -1,3 +1,4 @@
+<?php include "../_hidden/verify.php" ?>
 <!DOCTYPE html>
 <html lang="de">
 <head>
@@ -8,9 +9,9 @@
     <?php $styles[] = "kontakt"; include "../css/controller.php" ?>
 </head>
 <body>
-    <div id="part-1"><a href="mailto:contact@rindula.de?subject=Kontaktmail">Via E-Mail</a></div>
-    <div id="part-2"><a href="https://discord.gg/wHYgQxU">Auf meinem Discord Server</a></div>
-    <div id="part-3"><a href="ts3server://rindula.de?port=9987">Vielleicht bin ich auch auf meinem TS 3 Server...</a></div>
-    <div id="part-4"><a href="./lehrer">Oder doch lieber einen Lehrer?</a></div>
+    <div id="part-1"><a href="mailto:contact@rindula.de?subject=Kontaktmail" target="_blank">Via E-Mail</a></div>
+    <div id="part-2"><a href="https://discord.gg/wHYgQxU" target="_blank">Auf meinem Discord Server</a></div>
+    <div id="part-3"><a href="ts3server://rindula.de?port=9987" target="_blank">Vielleicht bin ich auch auf meinem TS 3 Server...</a></div>
+    <?php if ($loggedIn) ?><div id="part-4"><a href="./lehrer">Oder doch lieber einen Lehrer?</a></div><?php ; ?>
 </body>
 </html>
