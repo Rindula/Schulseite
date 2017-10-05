8806522c5c590bc5b435b0f24ceaed0e76e0bc21
Unknown <contact@rindula.de>
2017-10-05 23:56:49 +0200

    Kontakt Seite bearbeitet

--- kontakt/index.php
+++ kontakt/index.php
@@ -1,40 +1,16 @@
-<?php
-    //$exitLink = "/hausaufgaben/show";
-    $needVerify = false;
-
-    // Verifikation des Clients
-	include "../_hidden/verify.php";
-	
-    // Navigationsleiste
-    $pageId = 3;
-	include "../navbar.php";
-
-    // Global Header
-	include "../header.php";
-
-    // Benötigte Variablen
-	include "../_hidden/vars.php";
-
-    // CSS Controller
-    $styles[] = "kontakt";
-    include "../css/controller.php";
-
-    // Datenbankverbindung herstellen
-    mysql_connect("localhost", "rindula", $mySqlPassword);
-	// Datenbank und Tabelle erstellen, falls noch nicht vorhanden
-	mysql_query("CREATE DATABASE IF NOT EXISTS homeworks");
-	// Datenbank auswählen
-	mysql_select_db("homeworks");
-        mysql_query("SET NAMES 'utf8'");
-
-    echo "<table><tr><th>Name</th><th>Vorname</th><th>E-Mail</th></tr>";
-    $result = mysql_query("SELECT * FROM lehrer ORDER BY name Asc");
-    while ($ar = mysql_fetch_array($result)) 
-    {
-        $vorname = sonderzeichen($ar["vorname"]);
-        $name = sonderzeichen($ar["name"]);
-        $email = strtolower($vorname).".".strtolower($name)."@friedrich-hecker-schule.de";
-        echo "<tr><td>".$ar["name"]."</td><td>".$ar["vorname"]."</td><td><a href='mailto:".$email."'>".$email."</a></td></tr>";
-    }
-    echo "</table>";
-?>
+<!DOCTYPE html>
+<html lang="de">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Kontakt</title>
+    <?php $styles[] = "kontakt"; include "../css/controller.php" ?>
+</head>
+<body>
+    <div id="part-1"><a href="mailto:contact@rindula.de?subject=Kontaktmail">Via E-Mail</a></div>
+    <div id="part-2"><a href="https://discord.gg/wHYgQxU">Auf meinem Discord Server</a></div>
+    <div id="part-3"><a href="ts3server://rindula.de?port=9987">Vielleicht bin ich auch auf meinem TS 3 Server...</a></div>
+    <div id="part-4"><a href="./lehrer">Oder doch lieber einen Lehrer?</a></div>
+</body>
+</html>
