1c2f40f915b198566ca7aaff93891c9df38dead1
Rindula <contact@rindula.de>
2017-10-15 22:46:22 +0200

    Logout Fix

--- _hidden/logout.php
+++ _hidden/logout.php
@@ -5,5 +5,5 @@ session_destroy();
 $pageId = 3;
 include "../navbar.php";
  
-echo 'Logout erfolgreich<meta http-equiv="refresh" content="0; URL='.$_SESSION["url"].'">';
+echo 'Logout erfolgreich<meta http-equiv="refresh" content="0; URL=/hausaufgaben">';
 ?>
