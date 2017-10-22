067c132b27147ba0051f9f7f6564d6353a67796e
Rindula <contact@rindula.de>
2017-10-22 14:40:21 +0200

    ---

--- apis/haapi.php
+++ apis/haapi.php
@@ -17,7 +17,7 @@ try {
         $dbname = "homeworks";
         include "../_hidden/mysqlconn.php";
         try {
-            $sqlHausaufgaben = "SELECT h.ID, h.Aufgaben, h.Datum f.fach FROM list as h inner join flist as f on h.Fach = f.id ORDER BY Datum Asc";
+            $sqlHausaufgaben = "SELECT h.ID, h.Aufgaben, h.Datum f.fach FROM list as h inner join flist as f on h.Fach = f.id ORDER BY h.Datum Asc";
             $resultHausaufgaben = $mysqli->query($sqlHausaufgaben);
             logger("Hausaufgaben\n\n");
             if (gettype($resultHausaufgaben) == "boolean") {
