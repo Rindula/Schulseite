370cff24263e1bb6635457f8a56d896924ab55c1
Rindula <contact@rindula.de>
2018-01-09 16:38:30 +0100

    News Edit

--- news.php
+++ news.php
@@ -1,13 +1,12 @@
 <?php
 
 
-$newsConn = new mysqli("localhost", "root", "74cb0A0kER", "stats");
+$newsConn = @new mysqli("localhost", "root", "74cb0A0kER", "stats");
 if ($newsConn->connect_errno) {
     die("<span>Verbindung fehlgeschlagen: " . $newsConn->connect_error . "</span>");
 }
 $sql = "SET NAMES 'utf8'";
-$statement = $newsConn->prepare($sql);
-$statement->execute();
+$newsConn->query($sql);
 
 if($ret = $newsConn->query("SELECT * FROM news WHERE DATE(expdate) >= DATE(now())") !== FALSE) {
 
