7ff3f6700b37e7fcb1f93df4cc28e2bab7dd761d
Rindula <contact@rindula.de>
2018-03-02 19:06:17 +0100

    News Fix

--- news.php
+++ news.php
@@ -1,14 +1,14 @@
 <?php
 
 
-$newsConn = @new mysqli("localhost", "root", "74cb0A0kER", "stats");
+$newsConn = new mysqli("localhost", "root", "74cb0A0kER", "stats");
 if ($newsConn->connect_errno) {
     die("<span>Verbindung fehlgeschlagen: " . $newsConn->connect_error . "</span>");
 }
 $sql = "SET NAMES 'utf8'";
 $newsConn->query($sql);
 
-if($ret = $newsConn->query("SELECT * FROM news WHERE DATE(expdate) >= DATE(now())") !== FALSE) {
+if($ret = $newsConn->query("SELECT * FROM news WHERE DATE(expdate) >= DATE(now())")) {
 
     while ($news = $ret->fetch_assoc()) {
         echo "<span class='aktiv' id='message_".$news["id"]."'>" . $news["text"] . "</span>";
