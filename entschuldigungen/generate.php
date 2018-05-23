fc6d1d66d7265a6086e4c2ca8b10f5d0e8dbe52c
Rindula <contact@rindula.de>
2018-05-23 15:25:20 +0200

    ...

--- entschuldigungen/generate.php
+++ entschuldigungen/generate.php
@@ -54,8 +54,8 @@ $cor = imagecolorallocate($rImg, 0, 0, 0);
 imagettftext($rImg, 40, 0, 620, 280, $cor, "arial.ttf", $name);
 imagettftext($rImg, 40, 0, 620, 360, $cor, "arial.ttf", $klasse);
 imagettftext($rImg, 40, 0, 940, 360, $cor, "arial.ttf", $lehrer);
-imagettftext($rImg, 40, 0, 670, 450, $cor, "arial.ttf", date("d.m.Y", $date_von));
-imagettftext($rImg, 40, 0, 1040, 450, $cor, "arial.ttf", date("d.m.Y", $date_bis));
+imagettftext($rImg, 40, 0, 670, 450, $cor, "arial.ttf", date("d.m.Y", strtotime($date_von)));
+imagettftext($rImg, 40, 0, 1040, 450, $cor, "arial.ttf", date("d.m.Y", strtotime($date_bis)));
 imagettftext($rImg, 40, 0, 230, 570, $cor, "arial.ttf", $grund);
 imagettftext($rImg, 40, 0, 1410, 560, $cor, "arial.ttf", dateDiff($date_von, $date_bis));
  
