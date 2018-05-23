20c9f66365c567136c359cf6e27a9e0721cd6f69
Rindula <contact@rindula.de>
2018-05-24 00:55:11 +0200

    Linkreplace Funktion angepasst

--- _hidden/vars.php
+++ _hidden/vars.php
@@ -91,7 +91,7 @@ function replaceLink($text = "") {
 $reg_exUrl = "/(http|https|ftp|ftps)\:\/\/[a-zA-Z0-9\-\.]+\.[a-zA-Z]{2,3}(\/\S*)?/";
 if(preg_match($reg_exUrl, $text, $url)) {
 // make the urls hyper links
-return preg_replace($reg_exUrl, '<a href="'.$url[0].'" rel="nofollow">'.$url[0].'</a>', $text);
+return preg_replace($reg_exUrl, '<a href="'.$url[0].'" rel="nofollow" target="_blank">'.$url[0].'</a>', $text);
 } else {
 // if no urls in the text just return the text
 return $text;
