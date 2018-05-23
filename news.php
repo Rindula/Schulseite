199f486105f9e14767c28a8fb1fa735ec37cfd1a
Rindula <contact@rindula.de>
2018-05-24 00:44:56 +0200

    News Update

--- news.php
+++ news.php
@@ -7,10 +7,10 @@ $sql = "SET NAMES 'utf8'";
 $dbh->query($sql);
 
 foreach ($dbh->query('SELECT * FROM news WHERE expdate >= now() AND showdate <= now() AND needLogin = 0') as $news) {
-    echo "<div class='alert alert-info fade show' id='message_".$news["id"]."'>" . $news["text"] . "</div>";
+    echo "<div class='alert alert-info fade show' id='message_".$news["id"]."'><b>[".date("d.m.Y H:i", strtotime($news["showdate"]))."]</b><br>" . $news["text"] . "</div>";
 }
 if ($loggedIn) {
     foreach ($dbh->query('SELECT * FROM news WHERE expdate >= now() AND showdate <= now() AND needLogin = 1') as $news) {
-        echo "<div class='alert alert-info fade show' id='message_".$news["id"]."'>" . $news["text"] . "</div>";
+        echo "<div class='alert alert-info fade show' id='message_".$news["id"]."'><b>[".date("d.m.Y H:i", strtotime($news["showdate"]))."]</b><br>" . $news["text"] . "</div>";
     }
 }
