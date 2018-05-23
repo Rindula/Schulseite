ed4ae0f697615535c0a8b00ffb65f079ca8f7858
Rindula <contact@rindula.de>
2018-05-23 15:13:51 +0200

    ...

--- entschuldigungen/generate.php
+++ entschuldigungen/generate.php
@@ -11,16 +11,16 @@ $grund = $_GET["grund"];
 $rImg = imagecreatefrompng("form.png");
  
 //define color
-$cor = imagecolorallocate($rImg, 255, 255, 255);
+$cor = imagecolorallocate($rImg, 0, 0, 0);
  
 //define image, font style, location of string, String, color.
-imagestring($rImg,5,620,240,$name,$cor);
-imagestring($rImg,5,620,330,$klasse,$cor);
-imagestring($rImg,5,940,330,$lehrer,$cor);
-imagestring($rImg,5,670,430,$date_von,$cor);
-imagestring($rImg,5,1040,430,$date_bis,$cor);
-imagestring($rImg,5,230,550,$grund,$cor);
-imagestring($rImg,5,1410,540,$days,$cor);
+imagettftext($rImg, 16, 0, 620, 240, $cor, NULL, $name);
+imagettftext($rImg, 16, 0, 620, 330, $cor, NULL, $klasse);
+imagettftext($rImg, 16, 0, 940, 330, $cor, NULL, $lehrer);
+imagettftext($rImg, 16, 0, 670, 430, $cor, NULL, $date_von);
+imagettftext($rImg, 16, 0, 1040, 430, $cor, NULL, $date_bis);
+imagettftext($rImg, 16, 0, 230, 550, $cor, NULL, $grund);
+imagettftext($rImg, 16, 0, 1410, 540, $cor, NULL, $days);
  
 //Header output
 header('Content-type: image/png');
