456d8184ce28f8bcc4afd734400dcb4597d2a45d
Unknown <gamerbozz1@gmx.de>
2018-06-07 15:11:37 +0200

    Created the template for the Newspage

---
+++ news/createNews.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Nachrichten Erstellen</title>
+</head>
+
+<body class="container">
+<?php include "navbar.php" ?>
+<div>
+<h2 class="display-2">Changelog</h2>
+<div id="changelog"></div>
+</div>
+<?php include "_hidden/bottomScripts.php" ?>
+</body>
+</html>
