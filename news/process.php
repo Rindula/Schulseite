10b9215931b677593cabd5806574a1133da92af0
Rindula <contact@rindula.de>
2018-06-07 17:04:46 +0200

    // Debug

--- news/process.php
+++ news/process.php
@@ -1,15 +1,12 @@
 <?php 
 list($user, $pass) = array('root', '74cb0A0kER');
-$dbh = new PDO('mysql:host=localhost;dbname=homeworks', $user, $pass);
+$dbh = new PDO('mysql:host=localhost;dbname=stats', $user, $pass);
 
 $news = $_POST['news'];
 $showdate = $_POST['showdate'];
 $expdate = $_POST['expdate'];
 $needLogin = $_POST['needLogin'];
-echo $news.PHP_EOL;
-echo $showdate.PHP_EOL;
-echo $expdate.PHP_EOL;
-echo $needLogin.PHP_EOL;
+
 $stmt = $dbh->prepare("INSERT INTO news (`text`, `showdate`, `expdate`, `needLogin`) VALUES (:newsIN, :showdateIN, :expdateIN, :needLoginIN)");
 $stmt->bindParam(':newsIN', $news);
 $stmt->bindParam(':showdateIN', $showdate);
