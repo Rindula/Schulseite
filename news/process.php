2e3b6a3ac4ac4c09737ed4cbb4a98d59f54b1826
Rindula <contact@rindula.de>
2018-06-07 17:28:06 +0200

    Umlaute fix

--- news/process.php
+++ news/process.php
@@ -1,6 +1,7 @@
 <?php 
 list($user, $pass) = array('root', '74cb0A0kER');
 $dbh = new PDO('mysql:host=localhost;dbname=stats', $user, $pass);
+$dbh->query("SET NAMES utf8");
 
 $news = $_POST['news'];
 $showdate = $_POST['showdate'];
