2fb4c857681495131907ce0f789fec8bd67d2994
Rindula <contact@rindula.de>
2018-06-07 17:34:38 +0200

    Denkfehler behoben

--- news.php
+++ news.php
@@ -8,11 +8,11 @@ $dbh = new PDO('mysql:host=localhost;dbname=stats', $user, $pass);
 $sql = "SET NAMES 'utf8'";
 $dbh->query($sql);
 
-foreach ($dbh->query('SELECT * FROM news WHERE expdate >= adddate(now(), interval 22 hour) AND showdate <= now() AND needLogin = 0') as $news) {
+foreach ($dbh->query('SELECT * FROM news WHERE expdate >= adddate(now(), interval -22 hour) AND showdate <= now() AND needLogin = 0') as $news) {
     echo "<div class='alert alert-info fade show' id='message_".$news["id"]."'><b>[".date("d.m.Y", strtotime($news["showdate"]))."]</b><br>" . replaceLink($news["text"]) . "</div>";
 }
 if ($loggedIn) {
-    foreach ($dbh->query('SELECT * FROM news WHERE expdate >= adddate(now(), interval 22 hour) AND showdate <= now() AND needLogin = 1') as $news) {
+    foreach ($dbh->query('SELECT * FROM news WHERE expdate >= adddate(now(), interval -22 hour) AND showdate <= now() AND needLogin = 1') as $news) {
         echo "<div class='alert alert-info fade show' id='message_".$news["id"]."'><b>[".date("d.m.Y", strtotime($news["showdate"]))."]</b><br>" . replaceLink($news["text"]) . "</div>";
     }
 }
