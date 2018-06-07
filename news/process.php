ec5dbcc7fbfee432e4635e46a343e64f9bedc798
Rindula <contact@rindula.de>
2018-06-07 15:58:05 +0200

    Fixed entry

--- news/process.php
+++ news/process.php
@@ -2,16 +2,19 @@
 list($user, $pass) = array('root', '74cb0A0kER');
 $dbh = new PDO('mysql:host=localhost;dbname=homeworks', $user, $pass);
 
+$news = $_post['news'];
+$showdate = $_post['showdate'];
+$expdate = $_post['expdate'];
+$needLogin = $_post['needLogin'];
+
 $stmt = $dbh->prepare("INSERT INTO news (text, showdate, expdate, needLogin) VALUES (:news , :showdate , :expdate , :needLogin)");
 $stmt->bindParam(':news', $news);
 $stmt->bindParam(':showdate', $showdate);
 $stmt->bindParam(':expdate', $expdate);
 $stmt->bindParam(':needLogin', $needLogin);
 
-$news = $_post['news'];
-$showdate = $_post['showdate'];
-$expdate = $_post['expdate'];
-$needLogin = $_post['needLogin'];
 $stmt->execute(); 
 
-header("Location: ../hausaufgaben");?>
+header("Location: ../hausaufgaben");
+
+?>
