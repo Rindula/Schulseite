ba1a4eb4b763d65a93dcd0d3aa509ebe7afa326b
Rindula <contact@rindula.de>
2018-06-07 21:12:32 +0200

    Script addition

--- admin/usercontroll.php
+++ admin/usercontroll.php
@@ -1,12 +1,29 @@
+<?php
+//$exitLink = "/hausaufgaben/show";
+$needVerify = false;
+
+// Verifikation des Clients
+include "../_hidden/verify.php";
+
+// Navigationsleiste
+$page = "hausaufgaben";
+include "../navbar.php";
+
+// Global Header
+$title="Benutzerverwaltung";
+include "../header.php";
+
+// Benötigte Variablen
+include "../_hidden/vars.php";
+
+// CSS Controller
+include "../css/controller.php";
+
+
+?>
 <!doctype html>
 <html lang="de">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
-
-        <title><?= $title ?></title>
-    </head>
+    
     <body class="container">
         <form class="form form-control" method="post">
         <ul class="list-group list-group-flush">
@@ -18,14 +35,14 @@
 
         $gruppen = array();
 
-        foreach ($dbh->query('SELECT id, displayName FROM groups') as $row) {
+        foreach ($dbh->query('SELECT id, displayName FROM groups ORDER BY id') as $row) {
             $gruppen[] = array($row["id"], $row["displayName"]);
         }
 
         foreach ($dbh->query('SELECT name, vorname, id, email, gruppe FROM users ORDER BY name') as $row) {
             echo "
             <li class='list-group-item'>
-                <div class='float-left'>
+                <div class='form-group float-left'>
                     <span id='name'>".$row["name"].", ".$row["vorname"]."</span><br>
                     <span id='email'>".$row["email"]."</span>
                 </div>
@@ -43,8 +60,6 @@
         ?>
         </ul>
         </form>
-        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>
-        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script>
+        <?php include "../_hidden/bottomScripts.php" ?>
     </body>
 </html>
