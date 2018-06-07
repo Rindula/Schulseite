c6cf9c82c7f30abd41fb4c0e0531b5219969ffc5
Unknown <gamerbozz1@gmx.de>
2018-06-07 15:48:54 +0200

    Made it Work

---
+++ news/process.php
@@ -0,0 +1,15 @@
+<?php $needVerify = false; include "../_hidden/verify.php"; include "../_hidden/vars.php"; $darkMode = ($_COOKIE["darkmode"] == "true") ? true : false; 
+list($user, $pass) = array('root', '74cb0A0kER');
+$dbh = new PDO('mysql:host=localhost;dbname=homeworks', $user, $pass);
+
+$stmt = $dbh->prepare("INSERT INTO news (text, showdate, expdate, needLogin) VALUES (:news , :showdate , :expdate , :needLogin)");
+$stmt->bindParam(':news', $news);
+$stmt->bindParam(':showdate', $showdate);
+$stmt->bindParam(':expdate', $expdate);
+$stmt->bindParam(':needLogin', $needLogin);
+
+$news = $_post['news'];
+$showdate = $_post['showdate'];
+$expdate = $_post['expdate'];
+$needLogin = $_post['needLogin'];
+$stmt->execute(); ?>
