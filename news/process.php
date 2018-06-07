f5f41cb3d6dd480dfd375bf047c5f2e9f0a0c8d2
Rindula <contact@rindula.de>
2018-06-07 16:40:10 +0200

    Debug ...

--- news/process.php
+++ news/process.php
@@ -6,17 +6,15 @@ $news = $_post['news'];
 $showdate = $_post['showdate'];
 $expdate = $_post['expdate'];
 $needLogin = $_post['needLogin'];
-
+echo($news.PHP_EOL);
+echo($showdate.PHP_EOL);
+echo($expdate.PHP_EOL);
+echo($needLogin.PHP_EOL);
 $stmt = $dbh->prepare("INSERT INTO news (text, showdate, expdate, needLogin) VALUES (:news , :showdate , :expdate , :needLogin)");
 $stmt->bindParam(':news', $news);
 $stmt->bindParam(':showdate', $showdate);
 $stmt->bindParam(':expdate', $expdate);
 $stmt->bindParam(':needLogin', $needLogin);
-
-echo($news.PHP_EOL);
-echo($showdate.PHP_EOL);
-echo($expdate.PHP_EOL);
-echo($needLogin.PHP_EOL);
 
 $stmt->execute(); 
 
