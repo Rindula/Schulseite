3cd7139116b097ddaae3421524caf04d30833ab9
Rindula <contact@rindula.de>
2018-06-21 20:33:04 +0200

    onClick event hinzugefügt

--- news.php
+++ news.php
@@ -8,11 +8,15 @@ $dbh = new PDO('mysql:host=localhost;dbname=stats', $user, $pass);
 $sql = "SET NAMES 'utf8'";
 $dbh->query($sql);
 
+$onclick = "onclick='removeNews(this)'";
+
 foreach ($dbh->query('SELECT * FROM news WHERE expdate >= adddate(now(), interval -22 hour) AND showdate <= now() AND needLogin = 0') as $news) {
-    echo "<div class='alert alert-info fade show' id='message_".$news["id"]."'><b>[".date("d.m.Y", strtotime($news["showdate"]))."]</b><br>" . replaceLink($news["text"]) . "</div>";
+    echo "<div $onclick class='alert alert-info fade show' id='message_".$news["id"]."'><b>[".date("d.m.Y", strtotime($news["showdate"]))."]</b><br>" . replaceLink($news["text"]) . "</div>";
+    setcookie("message_".$news["id"], "true", strtotime($news["expdate"]) + (60*60*22));
 }
 if ($loggedIn) {
     foreach ($dbh->query('SELECT * FROM news WHERE expdate >= adddate(now(), interval -22 hour) AND showdate <= now() AND needLogin = 1') as $news) {
-        echo "<div class='alert alert-info fade show' id='message_".$news["id"]."'><b>[".date("d.m.Y", strtotime($news["showdate"]))."]</b><br>" . replaceLink($news["text"]) . "</div>";
+        echo "<div $onclick class='alert alert-info fade show' id='message_".$news["id"]."'><b>[".date("d.m.Y", strtotime($news["showdate"]))."]</b><br>" . replaceLink($news["text"]) . "</div>";
+        setcookie("message_".$news["id"], "true", strtotime($news["expdate"]) + (60*60*22));
     }
 }
