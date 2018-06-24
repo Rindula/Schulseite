172f4906c38bfb680ac673ab58a8b73792cc1623
Rindula <contact@rindula.de>
2018-06-24 17:20:30 +0200

    Discord Button geändert

--- navbar.php
+++ navbar.php
@@ -65,7 +65,7 @@
                 <a class="dropdown-item fa fa-bug<?= $dark ?>" id="bugtracker" target="_blank" href="https://github.com/rindula-de/Schulseite/issues/new?template=bug_report.md"> Bug melden</a>
                 <a class="dropdown-item<?= $dark ?>" target="_blank" href="https://discord.gg/wHYgQxU">
                     <i class="fa fa-gamepad"></i>
-                    <img class="img-fluid" src="/img/discord-banner-highres.gif" alt="Join my Discord Server">
+                    <img class="img-fluid" src="https://i.imgur.com/8vxTxZU.png" alt="Join my Discord Server">
                 </a>
                 <div class="dropdown-divider"></div>
                 <a class="dropdown-item<?= $dark ?>" id="datenschutz" href="/kontakt/datenschutz.php">Datenschutzerklärung</a>
