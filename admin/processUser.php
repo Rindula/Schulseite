c2628701e554b47324c2042aada1d7ba0203477b
Rindula <Rindula@users.noreply.github.com>
2018-07-05 09:51:20 +0200

    Update processUser.php

--- admin/processUser.php
+++ admin/processUser.php
@@ -1,17 +1,36 @@
 <?php
 
-$g = $_POST["gruppe"];
-$u = $_POST["user"];
+if (isset($_POST["user"])) {
 
-list($user, $pass) = array('query', 'Gen11!1y');
-$dbh = new PDO('mysql:host=localhost;dbname=stats', $user, $pass);
-$dbh->query('SET NAMES utf8');
+  $g = $_POST["gruppe"];
+  $u = $_POST["user"];
 
-$sth = $dbh->prepare("UPDATE users SET gruppe = :gruppe WHERE id = :userid");
+  list($user, $pass) = array('query', 'Gen11!1y');
+  $dbh = new PDO('mysql:host=localhost;dbname=stats', $user, $pass);
+  $dbh->query('SET NAMES utf8');
 
-$sth->bindParam(":gruppe", $g);
-$sth->bindParam(":userid", $u);
+  $sth = $dbh->prepare("UPDATE users SET gruppe = :gruppe WHERE id = :userid");
 
-$sth->execute();
+  $sth->bindParam(":gruppe", $g);
+  $sth->bindParam(":userid", $u);
+
+  $sth->execute();
+  
+}
+
+if (isset($_POST["resetPass"])) {
+  $u = $_POST["resetPass"];
+
+  list($user, $pass) = array('query', 'Gen11!1y');
+  $dbh = new PDO('mysql:host=localhost;dbname=stats', $user, $pass);
+  $dbh->query('SET NAMES utf8');
+
+  $sth = $dbh->prepare("UPDATE users SET password = :pass WHERE id = :userid");
+
+  $sth->bindParam(":pass", password_hash("default", PASSWORD_DEFAULT));
+  $sth->bindParam(":userid", $u);
+
+  $sth->execute();
+}
 
 header("Location: usercontroll.php");
