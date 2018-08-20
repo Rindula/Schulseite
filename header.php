ef143e3e713399f89ffe8843c6d6d12cefd68929
Rindula <contact@rindula.de>
2018-08-21 00:45:01 +0200

    Header fix

--- header.php
+++ header.php
@@ -2,16 +2,14 @@
 setlocale(LC_TIME, 'german', 'deu_deu', 'deu', 'de_DE', 'de');
 $pageTitle = "rindula.de";
 if (isset($title)) {
-	echo "<!DOCTYPE html>
-	<html>
+	echo "
 	<head>
 		<meta http-equiv='Content-Type' content='text/html; charset=UTF-8' />
 		<title>$title | $pageTitle</title>
 		<link rel='shortcut icon' href='/favicon.ico'>
 	</head>";
 } else {
-echo "<!DOCTYPE html>
-	<html>
+echo "
 	<head>
 		<meta http-equiv='Content-Type' content='text/html; charset=UTF-8' />
 		<title>$pageTitle</title>
