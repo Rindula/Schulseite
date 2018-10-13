90503251bfa8e26b64e410e3ff01579ad61ca032
Rindula <contact@rindula.de>
2018-10-13 23:13:29 +0200

    Fixed WeekCheck

--- scripts/weekcheck.php
+++ scripts/weekcheck.php
@@ -5,6 +5,6 @@ require_once "../../secrets.php";
 $dbh = new PDO('mysql:host=localhost;dbname=stats', DB_USER, DB_PASSWORD);
 $dbh->query('SET NAMES utf8');
 
-foreach ($dbh->query('SELECT value FROM configuration WHERE key = "weekConfig"') as $row) {
+foreach ($dbh->query('SELECT value FROM configuration WHERE ckey = "weekConfig"') as $row) {
     echo $row["value"];
 }
