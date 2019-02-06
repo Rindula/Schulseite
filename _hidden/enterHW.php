84911e82642f220ce4017115e9023fd608be8ed0
Rindula <contact@rindula.de>
2019-02-06 23:02:02 +0100

    Logging erweitert

--- _hidden/enterHW.php
+++ _hidden/enterHW.php
@@ -40,10 +40,12 @@ if (isset($_POST["type"])) {
     if ($_POST["type"] == "2") {
         $sql = "UPDATE `arbeiten` SET themen='$aufgabe', datum='$datum' WHERE id='$fach'";
         $mysqli->query($sql);
+        log_rin("ka_change","Klassenarbeit (ID: ". $fach .") geändert von " . $_SESSION["name"]);
     }
     if ($_POST["type"] == "3") {
         $sql = "UPDATE `list` SET Aufgaben='$aufgabe', Datum='$datum' WHERE ID='$fach'";
         $mysqli->query($sql);
+        log_rin("ka_change","Hausaufgabe (ID: ". $fach .") geändert von " . $_SESSION["name"]);
     }
     if ($_POST["type"] == "4") {
         $sql = "INSERT INTO `termine` (`raum`, `typ`, `datum`) VALUES ('$fach', '$aufgabe', '$datum $zeit')";
