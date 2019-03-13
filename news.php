c5f5adc6ee9b4368e1569c7f6c97a39684797939
Rindula <contact@rindula.de>
2019-03-13 21:48:36 +0100

    Disabled removal of the news banner

--- news.php
+++ news.php
@@ -8,23 +8,11 @@ $dbh = new PDO('mysql:host=localhost;dbname=stats', $user, $pass);
 $sql = "SET NAMES 'utf8'";
 $dbh->query($sql);
 
-$onclick = "onclick='removeNews(this)'";
-
 foreach ($dbh->query('SELECT * FROM news WHERE expdate >= adddate(now(), interval -22 hour) AND showdate <= now() AND needLogin = 0') as $news) {
-    if (!isset($_COOKIE["message_".$news["id"]])) {
-        setcookie("message_".$news["id"], "true", strtotime($news["expdate"]) + (60*60*22));
-    }
-    if ($_COOKIE["message_".$news["id"]] == "true") {
-        echo "<div $onclick class='alert alert-info fade show' id='message_".$news["id"]."'><b>[".date("d.m.Y", strtotime($news["showdate"]))."]</b><br>" . replaceLink($news["text"]) . "</div>";
-    }
+    echo "<div class='alert alert-info fade show' id='message_".$news["id"]."'><b>[".date("d.m.Y", strtotime($news["showdate"]))."]</b><br>" . replaceLink($news["text"]) . "</div>";
 }
 if ($loggedIn) {
     foreach ($dbh->query('SELECT * FROM news WHERE expdate >= adddate(now(), interval -22 hour) AND showdate <= now() AND needLogin = 1') as $news) {
-        if (!isset($_COOKIE["message_".$news["id"]])) {
-            setcookie("message_".$news["id"], "true", strtotime($news["expdate"]) + (60*60*22));
-        }
-        if ($_COOKIE["message_".$news["id"]] == "true") {
-            echo "<div $onclick class='alert alert-info fade show' id='message_".$news["id"]."'><b>[".date("d.m.Y", strtotime($news["showdate"]))."]</b><br>" . replaceLink($news["text"]) . "</div>";
-        }
+        echo "<div class='alert alert-info fade show' id='message_".$news["id"]."'><b>[".date("d.m.Y", strtotime($news["showdate"]))."]</b><br>" . replaceLink($news["text"]) . "</div>";
     }
 }
